10e37af5178284b44fa021678d8d3b9dcb73f703
Negar <negarhasani1379@gmail.com>
2026-08-03 17:20:23 +0330

    Register project apps

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'account',
+    'school',
+    'academic',
+    'finance',
 ]
 
 MIDDLEWARE = [
