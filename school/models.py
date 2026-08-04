53e9a65f96ac22d26cc7060df4c8c47385c2be2f
Negar <negarhasani1379@gmail.com>
2026-08-04 16:08:54 +0330

    feat(school): implement school model

--- school/models.py
+++ school/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from core.models import BaseModel
+
+
+class School(BaseModel):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
