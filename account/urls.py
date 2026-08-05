be00bc7e2d290e79dcdeb84d1ae219e5480fc2c0
Negar <negarhasani1379@gmail.com>
2026-08-06 03:18:11 +0330

    fix jwt

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from .views import MyTokenObtainPairView
 
 urlpatterns = [
-    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("login/", MyTokenObtainPairView.as_view(), name="login"),
     path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
