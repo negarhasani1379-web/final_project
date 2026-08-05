2f6d5a2388b03e2fb359028d169f0894ffc00604
Negar <negarhasani1379@gmail.com>
2026-08-05 15:22:56 +0330

    feat(account): customize custom user admin

--- account/admin.py
+++ account/admin.py
@@ -3,6 +3,57 @@ from django.contrib.auth.admin import UserAdmin
 
 from .models import User
 
+
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    pass
+   
+    list_display = (
+        "username",
+        "first_name",
+        "last_name",
+        "role",
+        "phone",
+        "is_active",
+    )
+
+    list_filter = (
+        "role",
+        "is_active",
+        "is_staff",
+        "is_superuser",
+    )
+
+    search_fields = (
+        "username",
+        "first_name",
+        "last_name",
+        "phone",
+    )
+
+    ordering = ("username",)
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Additional Information",
+            {
+                "fields": (
+                    "role",
+                    "phone",
+                    "emergency_phone",
+                ),
+            },
+        ),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Additional Information",
+            {
+                "fields": (
+                    "role",
+                    "phone",
+                    "emergency_phone",
+                ),
+            },
+        ),
+    )
