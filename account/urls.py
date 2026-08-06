4bcc0367f6702ae8b600dba161c61be652e8dd35
Negar <negarhasani1379@gmail.com>
2026-08-06 17:45:38 +0330

    Add role based permissions

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 
-from .views import MyTokenObtainPairView
+from .views import MyTokenObtainPairView, TeacherTestView
 
 urlpatterns = [
     path("login/", MyTokenObtainPairView.as_view(), name="login"),
     path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("teacher-test/", TeacherTestView.as_view(), name="teacher-test",),
 ]
