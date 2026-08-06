af5b65ebec5daaf195b20dd2a194a191f505b5b3
Negar <negarhasani1379@gmail.com>
2026-08-07 03:22:20 +0330

    create test of wrong password

--- account/tests.py
+++ account/tests.py
@@ -1,5 +1,4 @@
 from django.core.management import call_command
-from django.urls import reverse
 from rest_framework.test import APITestCase
 
 from .models import User
@@ -41,6 +40,17 @@ class JWTLoginTest(APITestCase):
 
         self.assertEqual(response.status_code, 401)
 
+    def test_login_wrong_username(self):
+        response = self.client.post(
+            "/api/auth/login/",
+            {
+                "username": "wrong_username",
+                "password": "12345678",
+            },
+            format="json",
+        )
+
+        self.assertEqual(response.status_code, 401)    
 
 class PermissionTest(APITestCase):
 
@@ -88,39 +98,3 @@ class CreateUserCommandTest(APITestCase):
         self.assertEqual(user.role, "teacher")
         self.assertEqual(user.phone, "09333333333")  
 
-
-class JWTLoginTest(APITestCase):
-
-    def setUp(self):
-        self.user = User.objects.create_user(
-            username="jwt_teacher",
-            password="12345678",
-            role="teacher",
-            phone="09333333335",
-        )
-
-    def test_login_success(self):
-        response = self.client.post(
-            "/api/auth/login/",
-            {
-                "username": "jwt_teacher",
-                "password": "12345678",
-            },
-            format="json",
-        )
-
-        self.assertEqual(response.status_code, 200)
-        self.assertIn("access", response.data)
-        self.assertIn("refresh", response.data)
-        
-    def test_login_wrong_password(self):
-        response = self.client.post(
-            "/api/auth/login/",
-            {
-                "username": "jwt_teacher",
-                "password": "wrong_password",
-            },
-            format="json",
-        )
-
-        self.assertEqual(response.status_code, 401)
