84a3f3199b1929b8458ccb2842e0df03fce07b16
Negar <negarhasani1379@gmail.com>
2026-08-11 23:30:04 +0330

    fix term date validation for partial updates

--- academic/serializers.py
+++ academic/serializers.py
@@ -15,8 +15,15 @@ class TermSerializer(serializers.ModelSerializer):
         )
 
     def validate(self, attrs):
-        start_date = attrs.get("start_date")
-        end_date = attrs.get("end_date")
+        start_date = attrs.get(
+            "start_date",
+            self.instance.start_date if self.instance else None,
+        )
+
+        end_date = attrs.get(
+            "end_date",
+            self.instance.end_date if self.instance else None,
+        )
 
         if start_date and end_date and end_date < start_date:
             raise serializers.ValidationError(
