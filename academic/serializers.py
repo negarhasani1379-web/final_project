7161446f4fa3fc5bb505a027e9d42835d8c48f6e
Negar <negarhasani1379@gmail.com>
2026-08-11 22:54:31 +0330

    add term date validation

--- academic/serializers.py
+++ academic/serializers.py
@@ -13,3 +13,14 @@ class TermSerializer(serializers.ModelSerializer):
             "end_date",
             "term_type",
         )
+
+    def validate(self, attrs):
+        start_date = attrs.get("start_date")
+        end_date = attrs.get("end_date")
+
+        if start_date and end_date and end_date < start_date:
+            raise serializers.ValidationError(
+                "End date cannot be before start date."
+            )
+
+        return attrs
