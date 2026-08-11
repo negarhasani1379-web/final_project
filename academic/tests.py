f26555416177f13b463361c9a4db29c63b9b86be
Negar <negarhasani1379@gmail.com>
2026-08-11 19:54:37 +0330

    test valid term creation

--- academic/tests.py
+++ academic/tests.py
@@ -1,3 +1,20 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Term
+
+
+class TermTest(TestCase):
+    def test_create_term_with_valid_dates(self):
+        term = Term(
+            title="Fall 2026",
+            start_date="2026-09-01",
+            end_date="2027-01-20",
+            term_type="normal",
+        )
+
+        term.full_clean()
+        term.save()
+
+        self.assertEqual(term.title, "Fall 2026")
+        self.assertEqual(term.term_type, "normal")
