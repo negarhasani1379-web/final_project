9d4ba262aee2660ce69497f3ee239c100d509dbf
Negar <negarhasani1379@gmail.com>
2026-08-13 02:58:38 +0330

    add teacher classes endpoint

--- academic/urls.py
+++ academic/urls.py
@@ -1,10 +1,23 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from .views import ClassViewSet, TeacherAssignmentViewSet, TermViewSet
+from .views import (
+    ClassViewSet,
+    TeacherAssignmentViewSet,
+    TeacherClassListView,
+    TermViewSet,
+)
 
 router = DefaultRouter()
 router.register("terms", TermViewSet, basename="term")
 router.register("classes",ClassViewSet,basename="class",)
 router.register("teacher-assignments",TeacherAssignmentViewSet,basename="teacher-assignment",)
 
-urlpatterns = router.urls
+
+urlpatterns = router.urls + [
+    path(
+        "teacher-classes/",
+        TeacherClassListView.as_view(),
+        name="teacher-classes",
+    ),
+]
