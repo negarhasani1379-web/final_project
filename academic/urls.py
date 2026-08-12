78bf6a54f5f0a12dfdda6c027cf0d75131394ed8
Negar <negarhasani1379@gmail.com>
2026-08-12 17:51:47 +0330

    add teacher assignment api

--- academic/urls.py
+++ academic/urls.py
@@ -1,9 +1,10 @@
 from rest_framework.routers import DefaultRouter
 
-from .views import ClassViewSet, TermViewSet
+from .views import ClassViewSet, TeacherAssignmentViewSet, TermViewSet
 
 router = DefaultRouter()
 router.register("terms", TermViewSet, basename="term")
 router.register("classes",ClassViewSet,basename="class",)
+router.register("teacher-assignments",TeacherAssignmentViewSet,basename="teacher-assignment",)
 
 urlpatterns = router.urls
