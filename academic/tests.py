7298403582d68d74a8892c155245fb38513d5992
Negar <negarhasani1379@gmail.com>
2026-08-12 04:21:33 +0330

    add class api tests

--- academic/tests.py
+++ academic/tests.py
@@ -8,6 +8,7 @@ from school.models import School
 
 from .models import Class, Term
 
+
 ########## TERM #########
 class TermTest(TestCase):
     def test_create_term_with_valid_dates(self):
@@ -383,4 +384,252 @@ class ClassTest(TestCase):
         )
 
         with self.assertRaises(ValidationError):
-            classroom.full_clean()            
+            classroom.full_clean() 
+
+class ClassAPITest(APITestCase):
+
+    def setUp(self):
+        self.education = User.objects.create_user(
+            username="education_class_test",
+            password="Test12345",
+            role="education",
+        )
+
+        self.teacher = User.objects.create_user(
+            username="teacher_class_test",
+            password="Test12345",
+            role="teacher",
+        )
+
+        self.school = School.objects.create(
+            name="Test School",
+        )
+
+        self.term = Term.objects.create(
+            title="Fall 2026",
+            start_date="2026-09-01",
+            end_date="2027-01-20",
+            term_type="normal",
+        )
+
+        self.class_data = {
+            "title": "English Literature",
+            "school": self.school.id,
+            "term": self.term.id,
+            "session_duration": 90,
+        }
+
+        self.url = "/api/classes/"
+
+    def test_education_can_create_class(self):
+        self.client.force_authenticate(user=self.education)
+
+        response = self.client.post(
+            self.url,
+            self.class_data,
+            format="json",
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_201_CREATED,
+        )
+
+        self.assertEqual(Class.objects.count(), 1)
+        self.assertEqual(
+            response.data["title"],
+            "English Literature",
+        )
+        self.assertEqual(
+            response.data["session_duration"],
+            90,
+        )
+
+    def test_teacher_cannot_create_class(self):
+        self.client.force_authenticate(user=self.teacher)
+
+        response = self.client.post(
+            self.url,
+            self.class_data,
+            format="json",
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_403_FORBIDDEN,
+        )
+
+        self.assertEqual(Class.objects.count(), 0)
+
+    def test_unauthenticated_user_cannot_create_class(self):
+        response = self.client.post(
+            self.url,
+            self.class_data,
+            format="json",
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_401_UNAUTHORIZED,
+        )
+
+        self.assertEqual(Class.objects.count(), 0)
+
+    def test_education_can_list_classes(self):
+        self.client.force_authenticate(user=self.education)
+
+        Class.objects.create(
+            title="English Literature",
+            school=self.school,
+            term=self.term,
+            session_duration=90,
+        )
+
+        response = self.client.get(self.url)
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK,
+        )
+
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(
+            response.data[0]["title"],
+            "English Literature",
+        )
+
+    def test_education_can_retrieve_class(self):
+        self.client.force_authenticate(user=self.education)
+
+        classroom = Class.objects.create(
+            title="English Literature",
+            school=self.school,
+            term=self.term,
+            session_duration=90,
+        )
+
+        response = self.client.get(
+            f"{self.url}{classroom.id}/"
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK,
+        )
+
+        self.assertEqual(
+            response.data["id"],
+            classroom.id,
+        )
+
+        self.assertEqual(
+            response.data["title"],
+            "English Literature",
+        )
+
+    def test_education_can_update_class(self):
+        self.client.force_authenticate(user=self.education)
+
+        classroom = Class.objects.create(
+            title="English Literature",
+            school=self.school,
+            term=self.term,
+            session_duration=90,
+        )
+
+        response = self.client.patch(
+            f"{self.url}{classroom.id}/",
+            {
+                "title": "Advanced English Literature",
+            },
+            format="json",
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK,
+        )
+
+        classroom.refresh_from_db()
+
+        self.assertEqual(
+            classroom.title,
+            "Advanced English Literature",
+        )
+
+    def test_update_class_with_invalid_session_duration(self):
+        self.client.force_authenticate(user=self.education)
+
+        classroom = Class.objects.create(
+            title="English Literature",
+            school=self.school,
+            term=self.term,
+            session_duration=90,
+        )
+
+        response = self.client.patch(
+            f"{self.url}{classroom.id}/",
+            {
+                "session_duration": 75,
+            },
+            format="json",
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_400_BAD_REQUEST,
+        )
+
+        classroom.refresh_from_db()
+
+        self.assertEqual(
+            classroom.session_duration,
+            90,
+        )
+
+    def test_education_can_soft_delete_class(self):
+        self.client.force_authenticate(user=self.education)
+
+        classroom = Class.objects.create(
+            title="English Literature",
+            school=self.school,
+            term=self.term,
+            session_duration=90,
+        )
+
+        response = self.client.delete(
+            f"{self.url}{classroom.id}/"
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_204_NO_CONTENT,
+        )
+
+        classroom.refresh_from_db()
+
+        self.assertTrue(classroom.is_deleted) 
+
+    def test_deleted_class_is_not_in_class_list(self):
+        self.client.force_authenticate(user=self.education)
+
+        classroom = Class.objects.create(
+            title="Deleted Class",
+            school=self.school,
+            term=self.term,
+            session_duration=90,
+        )
+
+        classroom.delete()
+
+        response = self.client.get(self.url)
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK,
+        )
+
+        self.assertEqual(
+            len(response.data),
+            0,
+        )           
