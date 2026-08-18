1180b2447a90d0badf95ba509b0aff255ffd88f4
Negar <negarhasani1379@gmail.com>
2026-08-18 19:16:46 +0330

    Add session report create view

--- finance/views.py
+++ finance/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from finance.serializers import SessionReportSerializer
+
+
+class SessionReportCreateView(generics.CreateAPIView):
+    serializer_class = SessionReportSerializer
+    permission_classes = [IsAuthenticated]
